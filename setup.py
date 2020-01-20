128ef2043ec616aba1f6fde3c572252b86d0bc41
ErikBuer <buer94@gmail.com>
2020-01-20 13:17:56 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ChirpAnalyzer',
+      version='0.0.1',
+      description='Masters Project',
+      url='https://github.com/ErikBuer/ChirpAnalyzer',
+      author='Erik Buer',
+      author_email='buer94@gmail.com',
+      license='',
+      packages=['ChirpAnalyzer'],
+      zip_safe=False,
+      install_requires     = [ 'numpy','scipy'])
