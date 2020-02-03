3e22682de00f045e1bcf5be7544fa10ebc1d6c0a
ErikBuer <buer94@gmail.com>
2020-02-03 16:06:28 +0100

    partially functioning. Functioning chebychev gamma_t generator

--- ChirpAnalyzer/signal_generator.py
+++ ChirpAnalyzer/signal_generator.py
@@ -12,25 +12,16 @@ frameSize=np.float(10e-3)  # seconds
 
 
 # Time domain window for the function to match
-chirp = radar.chirp(frameSize, Fs)
+chirp = radar.chirp(Fs)
 chirp.fftLen = 1024
 
-"""
-# Generate target autocorrelation window
-#window_t = signal.chebwin(512, 60)
-#r_xx = chirp.PSD(window_t)
-#r_xx = np.fft.fftshift(r_xx / abs(r_xx).max())
-#r_xx_dB = util.mag2db( r_xx )
-"""
-
 # Synthesize the target autocorrelation function
-window_t = signal.chebwin(4096, 45)
-r_xx = np.fft.fft(window_t, chirp.fftLen)
-r_xx = np.abs(np.fft.fftshift(r_xx / abs(r_xx).max()))
-r_xx_dB = util.mag2db(np.maximum(r_xx, 1e-10))
-chirp = radar.chirp(frameSize, Fs)
+window_t = signal.chebwin(np.intc(512), 60)
+plt.plot(window_t)
+plt.show()
 
-coeff = chirp.getCoefficients( r_xx_dB, order=8, symm=True)
+
+coeff = chirp.getCoefficients( window_t, order=8, symm=True)
 print( coeff )
 
 sig = chirp.generate()
