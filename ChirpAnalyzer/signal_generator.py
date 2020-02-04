6ea33f43a0bf64f9140789c154d7bfba374358b8
ErikBuer <buer94@gmail.com>
2020-02-04 16:25:41 +0100

    Added comparison with linear chirp

--- ChirpAnalyzer/signal_generator.py
+++ ChirpAnalyzer/signal_generator.py
@@ -7,24 +7,33 @@ import numpy as np
 import rftool.radar as radar
 import rftool.utility as util
 
-Fs=np.intc(500e3)           # receiver sample rate
-frameSize=np.float(10e-3)  # seconds
+Fs=np.intc(1.6e6) # receiver sample rate
+T=np.float(10e-3)  # Pulse duration
 
 
 # Time domain window for the function to match
-chirp = radar.chirp(Fs)
-chirp.fftLen = 1024
+NLFM = radar.chirp(Fs)
+NLFM.fftLen = 2048
 
 # Synthesize the target autocorrelation function
-window_t = signal.chebwin(np.intc(512), 60)
-plt.plot(window_t)
-plt.show()
+window_t = signal.chebwin(np.intc(2048), 60)
+#window_t = signal.gaussian(np.intc(2048), 360)
+#window_t = signal.gaussian(np.intc(2048), 400)
+NLFM.getCoefficients( window_t, targetBw=160e3, centerFreq=100e3, symm=True, T=T)
 
 
-coeff = chirp.getCoefficients( window_t, order=8, symm=True)
-print( coeff )
 
-sig = chirp.generate()
-#radar.ACF(sig, singleSided=False)
-radar.hilbert_spectrum(np.real(sig), Fs)
+# Time domain window for the function to match
+LFM = radar.chirp(Fs)
+LFM.fftLen = 2048
+
+# Synthesize the target autocorrelation function
+window_t = np.array([1,1,1])
+LFM.getCoefficients( window_t, targetBw=160e3, centerFreq=100e3, symm=True, T=T)
+
+signals = np.stack((LFM.sig, NLFM.sig), axis=-1)
+
+radar.ACF(signals)
+radar.hilbert_spectrum(np.real(LFM.sig), Fs)
+radar.hilbert_spectrum(np.real(NLFM.sig), Fs)
 plt.show()
