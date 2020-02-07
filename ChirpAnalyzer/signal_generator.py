78eeca0a6a651b1adaefb9268bc931e8b272f75c
ErikBuer <buer94@gmail.com>
2020-02-07 15:47:43 +0100

    Modulation example

--- ChirpAnalyzer/signal_generator.py
+++ ChirpAnalyzer/signal_generator.py
@@ -1,17 +1,16 @@
 from scipy.signal import chirp, sweep_poly, spectrogram, welch
 from scipy.special import factorial
 import scipy.signal as signal
-import analysis as analyze
 import matplotlib.pyplot as plt
 import numpy as np
 import rftool.radar as radar
 import rftool.utility as util
 
-Fs=np.intc(1.6e6) # receiver sample rate
-T=np.float(10e-3)  # Pulse duration
+Fs=np.intc(802e3) # receiver sample rate
+T=np.float(6e-3)  # Pulse duration
 
 
-# Time domain window for the function to match
+# Time domain window for NLFM generation
 NLFM = radar.chirp(Fs)
 NLFM.fftLen = 2048
 
@@ -19,34 +18,39 @@ NLFM.fftLen = 2048
 window_t = signal.chebwin(np.intc(2048), 60)
 #window_t = signal.gaussian(np.intc(2048), 360)
 #window_t = signal.gaussian(np.intc(2048), 400)
-NLFM.getCoefficients( window_t, targetBw=160e3, centerFreq=100e3, symm=True, T=T)
+NLFM.getCoefficients( window_t, targetBw=200e3, centerFreq=100e3, T=T)
 
 
 
-# Time domain window for the function to match
+# Time domain window for LFM generation
 LFM = radar.chirp(Fs)
-LFM.fftLen = 2048
 
-# Synthesize the target autocorrelation function
+# Generate linear chirp
 window_t = np.array([1,1,1])
-LFM.getCoefficients( window_t, targetBw=160e3, centerFreq=100e3, symm=True, T=T)
+LFM.getCoefficients( window_t, targetBw=200e3, centerFreq=100e3, T=T)
 
-signals = np.stack((LFM.sig, NLFM.sig), axis=-1)
+""""
+LFMsig = LFM.genFromPoly()
+NLFMsig = NLFM.genFromPoly()
+signals = np.stack((LFMsig, NLFMsig), axis=-1)
 
 radar.ACF(signals, label=['LFM', 'NLFM'])
-radar.hilbert_spectrum(np.real(LFM.sig), Fs, label='LFM')
-radar.hilbert_spectrum(np.real(NLFM.sig), Fs, label='NLFM')
-plt.show()
+radar.hilbert_spectrum(np.real(LFMsig), Fs, label='LFM')
+radar.hilbert_spectrum(np.real(NLFMsig), Fs, label='NLFM')
 
+radar.hilbert_spectrum(np.real(NLFM.modulate([1,1,1,1,0,1,0,1])), Fs, label='NLFM')
+"""
+NLFM.demodulate(NLFM.modulate())
+
+plt.show()
 
 
 """
 TODO Generation
-List of parameters to estimate on single chirp
-
-- Try polyfit med standard polygon.
+- Modulate signal
 
 TODO Analyze
+- List of parameters to estimate on single chirp
 - Skriv i rapport hvordan HH-transform foregår.
 - Lag cyclostationary transform.
 - Vurder Om kurvene skal karakteriseres fra sentrum, eller frs siden (i tid-frekvens).
