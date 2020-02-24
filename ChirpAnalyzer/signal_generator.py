1f347d00b89887e36587a11ab22e50ee3d23ff34
ErikBuer <buer94@gmail.com>
2020-02-24 15:09:06 +0100

    functioning Bit rate estimator

--- ChirpAnalyzer/signal_generator.py
+++ ChirpAnalyzer/signal_generator.py
@@ -19,34 +19,36 @@ NLFM.fftLen = 2048
 window_t = signal.hamming(np.intc(2048))
 #window_t = signal.gaussian(np.intc(2048), 360)
 #window_t = signal.gaussian(np.intc(2048), 400)
-NLFM.getCoefficients( window_t, targetBw=200e3, centerFreq=100e3, T=T)
+
+NLFM.getCoefficients( window_t, targetBw=100e3, centerFreq=100e3, T=T)
 
 
 """
-# Time domain window for LFM generation
 LFM = radar.chirp(Fs)
 
 # Generate linear chirp
 window_t = np.array([1,1,1])
 LFM.getCoefficients( window_t, targetBw=200e3, centerFreq=100e3, T=T)
 
-
-LFMsig = LFM.genFromPoly()
+LFMsig = LFM.genNumerical()
 NLFMsig = NLFM.genFromPoly()
 signals = np.stack((LFMsig, NLFMsig), axis=-1)
-
 
 radar.ACF(signals, label=['LFM', 'NLFM'])
 #radar.hilbert_spectrum(np.real(LFMsig), Fs, label='LFM')
 radar.hilbert_spectrum(np.real(NLFMsig), Fs, label='NLFM')
 """
 
-modSig = NLFM.modulate()
+package = np.random.randint(0, 1, 32)
+modSig = NLFM.modulate( package )
+modSig = util.wgnSnr( modSig, -59) #-53
+util.welch(modSig, Fs=Fs)
 
-radar.hilbert_spectrum(np.real(modSig), Fs, label='NLFM')
+#radar.hilbert_spectrum(np.real(modSig), Fs, label='NLFM')
 
 
-radar.FAM(modSig, Fs = Fs, plot=True)
+SCD, f, alpha = radar.FAM(modSig, Fs = Fs, plot=False, method='conj', scale='linear')
+radar.cyclicEstimator( SCD, f, alpha )
 
 plt.show()
 
@@ -54,11 +56,15 @@ plt.show()
 """
 TODO Generation
 - Generate binary files
-- Add noise
 
 TODO Analyze
+- Time domain multiplication of frequency fomain triangle window convolution. For IF and symbol rate estimation.
+- Bandwidth estimation
+
 - List of parameters to estimate on single chirp
 - Skriv i rapport hvordan HH-transform foregår.
-- Lag cyclostationary transform.
 - Vurder Om kurvene skal karakteriseres fra sentrum, eller frs siden (i tid-frekvens).
+
+TODO Report
+- How chich parameters in FAM decides the estimation resolution?
 """
