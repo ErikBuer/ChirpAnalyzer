867d29911cbe22953ca24c34d73e0734f31e73a5
ErikBuer <buer94@gmail.com>
2020-03-15 14:43:14 +0100

    Functioning plot of estimation error

--- ChirpAnalyzer/analysis.py
+++ ChirpAnalyzer/analysis.py
@@ -4,11 +4,22 @@ import scipy.signal as signal
 import numpy as np
 import random
 
-import matplotlib.pyplot as plt
-from matplotlib import cm
-
 import rftool.radar as radar
 import rftool.utility as util
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import matplotlib
+
+matplotlib.use("pgf")
+matplotlib.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+})
+
+import joblib   # Parallelizations
 import pickle
 from waveform import *
 import os
@@ -26,18 +37,17 @@ def EbN0toSNRdB(EbN0, M, Fs, Fsymb):
 
 
 # find number of files in folder
-path = '../../waveforms/'
-
-
-path, dirs, files = os.walk(path).__next__()
+directory = '../../waveforms/'
+path, dirs, files = os.walk(directory).__next__()
 
 file_count = len(files)
 nIterations = file_count
 
 Fs = np.intc(802e3) # Receiver sample rate. #! Must be the same as the signals
 T=np.float(6e-3)  # Pulse duration. #! Must be the same as the signals
-EbN0Vector = np.linspace(0, 40, 21)
+EbN0Vector = np.linspace(0, -30, 31)
 snrVector = EbN0toSNRdB(EbN0Vector, 2, Fs, 1/T)
+
 fCenterEstimate = np.zeros((nIterations, len(snrVector)), dtype=np.float64)
 R_symbEstimate = np.zeros((nIterations, len(snrVector)))
 
@@ -63,6 +73,23 @@ for i in range(0, nIterations):
     targetR_symb = 1/NLFM.T
 
 
+    def estimator(modSig, SNR, sigObj):
+        package = util.wgnSnr( modSig, SNR)
+
+        SCD, f, alpha = radar.FAM(package, Fs = sigObj.Fs, plot=False, method='conj', scale='linear')
+        fCenter, R_symb = radar.cyclicEstimator( SCD, f, alpha )
+
+        fCenterEstimate = np.abs(sigObj.fCenter-fCenter)
+        R_symbEstimate = np.abs(targetR_symb-R_symb)
+        return fCenterEstimate, R_symbEstimate
+
+    # = joblib.Parallel(n_jobs=8, verbose=20)(joblib.delayed(estimator)(modSig, SNR, sigObj) for SNR in snrVector)
+    estimates = joblib.Parallel(n_jobs=8, verbose=0)(joblib.delayed(estimator)(modSig, SNR, sigObj) for SNR in snrVector)
+    estimateMat = np.asarray(estimates)
+    fCenterEstimate[i,:] = estimateMat[:, 0]
+    R_symbEstimate[i,:] = estimateMat[:, 1]
+
+"""
     for j, SNR in enumerate(snrVector):
         package = util.wgnSnr( modSig, SNR)
 
@@ -71,26 +98,37 @@ for i in range(0, nIterations):
 
         fCenterEstimate[i,j] = np.abs(sigObj.fCenter-fCenter)
         R_symbEstimate[i,j] = np.abs(targetR_symb-R_symb)
-
+"""
 
 
 # Calculate MSE
 fCenterEstimateVector = np.mean(np.power(fCenterEstimate, 2), 0)
 R_symbEstimateVector = np.mean(np.power(R_symbEstimate, 2), 0)
 
+imagePath = "../figures/estimation/"
 
 plt.figure()
 plt.plot(EbN0Vector, fCenterEstimateVector)
+plt.grid()
 plt.title("Center Frequency Estimation Error")
-plt.xlabel('E_b/N_0')
+plt.xlabel('E_b/N_0 [dB]')
 plt.ylabel('MSE')
 plt.tight_layout()
 
+fileName = 'Center_Frequency_Estimation_Error' + str(nIterations)
+plt.savefig(imagePath + fileName + '.png', bbox_inches='tight')
+plt.savefig(imagePath + fileName + '.pgf', bbox_inches='tight')
+
 plt.figure()
 plt.plot(EbN0Vector, R_symbEstimateVector)
+plt.grid()
 plt.title("Symbol Rate Estimation Error")
-plt.xlabel('E_b/N_0')
+plt.xlabel('E_b/N_0 [dB]')
 plt.ylabel('MSE')
 plt.tight_layout()
+
+fileName = 'Symbol_Rate_Estimation_Error' + str(nIterations)
+plt.savefig(imagePath + fileName + '.png', bbox_inches='tight')
+plt.savefig(imagePath + fileName + '.pgf', bbox_inches='tight')
 
 plt.show()
